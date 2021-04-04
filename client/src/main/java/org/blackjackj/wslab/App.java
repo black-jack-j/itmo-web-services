952b3d42dfef0c5c95aa87a6cdd90a1314c9bee6
Fitisov Dmitrii <fitisovdmtr@gmail.com>
2021-04-04 20:51:06 +0300

    Lab1-client:
    - add command line parsing
    - add readme

--- client/src/main/java/org/blackjackj/wslab/App.java
+++ client/src/main/java/org/blackjackj/wslab/App.java
@@ -1,5 +1,7 @@
 package org.blackjackj.wslab;
 
+import org.apache.commons.cli.*;
+import org.blackjackj.wslab.service.Product;
 import org.blackjackj.wslab.service.ProductService;
 import org.blackjackj.wslab.service.ProductWebService;
 
@@ -7,9 +9,64 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class App {
-    public static void main( String[] args ) throws MalformedURLException {
-        URL serviceUrl = new URL("http://localhost:8080/ProductService?wsdl");
-        ProductService productWebService = new ProductWebService(serviceUrl).getProductServicePort();
-        productWebService.getProducts(null, null, null, null, null).forEach(System.out::println);
+    public static void main( String[] args ) throws MalformedURLException, ParseException {
+        ProductService productService = getProductService();
+
+        final Options options = new Options();
+
+        options.addOption("i", "id", true, "filter by ID");
+        options.addOption("n", "name", true, "filter by NAME");
+        options.addOption("m", "manufacturer", true, "filter by MANUFACTURER");
+        options.addOption("p", "price", true, "filter by PRICE");
+        options.addOption("d", "description", true, "filter by DESCRIPTION");
+        options.addOption("h", "help", false, "show help");
+
+        CommandLineParser parser = new DefaultParser();
+
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption("h")) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("wslab-client", options);
+            return;
+        }
+
+        Long id = null;
+        String name = null, manufacturer = null, description = null;
+        Double price = null;
+
+        if (cmd.hasOption("i")) {
+            id = Long.parseLong(cmd.getOptionValue("i"));
+        }
+
+        if (cmd.hasOption("n")) {
+            name = cmd.getOptionValue("n");
+        }
+
+        if (cmd.hasOption("m")) {
+            manufacturer = cmd.getOptionValue("m");
+        }
+
+        if (cmd.hasOption("d")) {
+            description = cmd.getOptionValue("d");
+        }
+
+        if (cmd.hasOption("p")) {
+            price = Double.parseDouble(cmd.getOptionValue("p"));
+        }
+
+        productService.getProducts(id, name, manufacturer, price, description).stream().map(App::parseProduct).forEach(System.out::println);
     }
+
+    private static ProductService getProductService() throws MalformedURLException {
+        URL serviceUrl = new URL("http://localhost:8080/ProductService?wsdl");
+        return new ProductWebService(serviceUrl).getProductServicePort();
+    }
+
+    private static String parseProduct(Product product) {
+        return String.format("Product{id: %d, name: '%s', manufacturer: '%s', price: %f%s'}\n",
+                product.getId(), product.getName(), product.getManufacturer(), product.getPrice(),
+                product.getDescription() == null ? "" : String.format(", description: '%s", product.getDescription()));
+    }
+
 }
